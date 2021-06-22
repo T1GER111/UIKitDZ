7b9ce696dbb93fe0f6abca8a91936a1de39f04dd
Тигран Левонян <hp6x5nywzz@privaterelay.appleid.com>
2021-06-22 14:10:37 +0300

    init

---
+++ UIkitDZ/UIkitDZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIkitDZ
+//
+//  Created by T1GER on 22.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
